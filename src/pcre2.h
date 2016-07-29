5b6c797a4d05c79bf1fb46a52e1920d9b5482d28
Philip.Hazel <Philip.Hazel@gmail.com>
2016-07-29 08:47:39 +0000

    Final file tidies for 10.22.

--- src/pcre2.h
+++ src/pcre2.h
@@ -43,8 +43,8 @@ POSSIBILITY OF SUCH DAMAGE.
 
 #define PCRE2_MAJOR          10
 #define PCRE2_MINOR          22
-#define PCRE2_PRERELEASE     -RC1
-#define PCRE2_DATE           2016-06-29
+#define PCRE2_PRERELEASE     
+#define PCRE2_DATE           2016-07-29
 
 /* When an application links to a PCRE DLL in Windows, the symbols that are
 imported have to be identified as such. When building PCRE2, the appropriate
