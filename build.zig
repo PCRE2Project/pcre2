819f28865d8b80a4ea44528dd51cfc92d8a3cc79
Tristan Ross <tristan.ross@midstall.com>
2024-03-14 17:48:03 -0700

    Update build.zig to support latest master

--- build.zig
+++ build.zig
@@ -9,7 +9,7 @@ pub const CodeUnitWidth = enum {
 pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
-    const linkage = b.option(std.Build.Step.Compile.Linkage, "linkage", "whether to statically or dynamically link the library") orelse .static;
+    const linkage = b.option(std.builtin.LinkMode, "linkage", "whether to statically or dynamically link the library") orelse @as(std.builtin.LinkMode, if (target.result.isGnuLibC()) .dynamic else .static);
     const codeUnitWidth = b.option(CodeUnitWidth, "code-unit-width", "Sets the code unit width") orelse .@"8";
 
     const copyFiles = b.addWriteFiles();
@@ -45,36 +45,35 @@ pub fn build(b: *std.Build) !void {
 
     lib.addCSourceFiles(.{
         .files = &.{
-            b.pathFromRoot("src/pcre2_auto_possess.c"),
-            b.pathFromRoot("src/pcre2_chkdint.c"),
-            b.pathFromRoot("src/pcre2_compile.c"),
-            b.pathFromRoot("src/pcre2_config.c"),
-            b.pathFromRoot("src/pcre2_context.c"),
-            b.pathFromRoot("src/pcre2_convert.c"),
-            b.pathFromRoot("src/pcre2_dfa_match.c"),
-            b.pathFromRoot("src/pcre2_error.c"),
-            b.pathFromRoot("src/pcre2_extuni.c"),
-            b.pathFromRoot("src/pcre2_find_bracket.c"),
-            b.pathFromRoot("src/pcre2_maketables.c"),
-            b.pathFromRoot("src/pcre2_match.c"),
-            b.pathFromRoot("src/pcre2_match_data.c"),
-            b.pathFromRoot("src/pcre2_newline.c"),
-            b.pathFromRoot("src/pcre2_ord2utf.c"),
-            b.pathFromRoot("src/pcre2_pattern_info.c"),
-            b.pathFromRoot("src/pcre2_script_run.c"),
-            b.pathFromRoot("src/pcre2_serialize.c"),
-            b.pathFromRoot("src/pcre2_string_utils.c"),
-            b.pathFromRoot("src/pcre2_study.c"),
-            b.pathFromRoot("src/pcre2_substitute.c"),
-            b.pathFromRoot("src/pcre2_substring.c"),
-            b.pathFromRoot("src/pcre2_tables.c"),
-            b.pathFromRoot("src/pcre2_ucd.c"),
-            b.pathFromRoot("src/pcre2_valid_utf.c"),
-            b.pathFromRoot("src/pcre2_xclass.c"),
+            "src/pcre2_auto_possess.c",
+            "src/pcre2_chkdint.c",
+            "src/pcre2_compile.c",
+            "src/pcre2_config.c",
+            "src/pcre2_context.c",
+            "src/pcre2_convert.c",
+            "src/pcre2_dfa_match.c",
+            "src/pcre2_error.c",
+            "src/pcre2_extuni.c",
+            "src/pcre2_find_bracket.c",
+            "src/pcre2_maketables.c",
+            "src/pcre2_match.c",
+            "src/pcre2_match_data.c",
+            "src/pcre2_newline.c",
+            "src/pcre2_ord2utf.c",
+            "src/pcre2_pattern_info.c",
+            "src/pcre2_script_run.c",
+            "src/pcre2_serialize.c",
+            "src/pcre2_string_utils.c",
+            "src/pcre2_study.c",
+            "src/pcre2_substitute.c",
+            "src/pcre2_substring.c",
+            "src/pcre2_tables.c",
+            "src/pcre2_ucd.c",
+            "src/pcre2_valid_utf.c",
+            "src/pcre2_xclass.c",
         },
         .flags = &.{
             "-DHAVE_CONFIG_H",
-            "-DPCRE2_CODE_UNIT_WIDTH=8",
             "-DPCRE2_STATIC",
         },
     });
