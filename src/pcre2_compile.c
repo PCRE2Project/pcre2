0beb86d89631cd2afab7171c7a64a6edebaa1c58
Carlo Marcelo Arenas Belón <carenas@gmail.com>
2024-09-23 19:47:45 -0700

    doc: fix silly confusion about re "strict"
    in Perl, `use strict` has no effect on this feature but instead
    the "strict" mode referenced applies to `use re 'strict'`

--- src/pcre2_compile.c
+++ src/pcre2_compile.c
@@ -2060,8 +2060,8 @@ else
         /* Perl has the surprising/broken behaviour that \x without following
         hex digits is treated as an escape for NUL. Their source code laments
         this but keeps it for backwards compatibility. A warning is printed
-        when "use warnings" is enabled, and it's forbidden when "use strict"
-        is enabled. Because we don't have warnings, we simply forbid it. */
+        when "use warnings" is enabled. Because we don't have warnings, we
+        simply forbid it. */
         if (ptr >= ptrend || (cc = XDIGIT(*ptr)) == 0xff)
           {
           /* Not a hex digit */
@@ -2072,9 +2072,9 @@ else
         c = cc;
 
         /* With "use re 'strict'" Perl actually requires exactly two digits (error
-        for both \xA and \xAAA). This seems overly strict, and there seems
-        little incentive to align with that, given the backwards-compatibility
-        cost.
+        for \x, \xA and \xAAA). While \x was already rejected, this seems overly
+        strict, and there seems little incentive to align with that, given the
+        backwards-compatibility cost.
 
         For comparison, note that other engines disagree. For example:
           - Java allows 1 or 2 hex digits. Error if 0 digits. No error if >2 digits
