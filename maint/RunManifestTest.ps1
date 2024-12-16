60df6558a8cd5255e6d59d0f233ad206dea96f85
Nicholas Wilson <niwilson@microsoft.com>
2024-12-16 15:45:02 +0000

    Add CI test to ensure installation manifest is correct
    Fixes #617

---
+++ maint/RunManifestTest.ps1
@@ -0,0 +1,37 @@
+#/bin/sh
+
+# Script to test a directory listing. We use this to verify that the list of
+# files installed by "make install" or "cmake --install" matches what we expect.
+
+param (
+  [Parameter(Mandatory=$true)]
+  [string]$inputDir,
+
+  [Parameter(Mandatory=$true)]
+  [string]$manifestName
+)
+
+if ((-not $inputDir) -or (-not $manifestName)) {
+  throw "Usage: .\RunManifestTest.ps1 <dir> <manifest name>"
+}
+
+$base = [System.IO.Path]::GetFileName($manifestName)
+
+$installedFiles = Get-ChildItem -Recurse -Force -Path $inputDir |
+  Sort-Object {[System.BitConverter]::ToString([system.Text.Encoding]::UTF8.GetBytes($_.FullName))} |
+  ForEach-Object { $_.Mode.Substring(0,5) + " " + ($_.FullName | Resolve-Path -Relative) }
+
+$null = New-Item -Force $base -Value ($installedFiles | Out-String)
+
+$expectedFiles = Get-Content -Path $manifestName -Raw
+$actualFiles = Get-Content -Path $base -Raw
+
+if ($expectedFiles -ne $actualFiles) {
+  Write-Host "===Actual==="
+  Write-Host $actualFiles
+  Write-Host "===End==="
+
+  throw "Installed files differ from expected"
+}
+
+Write-Host "Installed files match expected"
