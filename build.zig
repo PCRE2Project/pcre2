7b643c83f06a9cde461f97a3824ed8d4f4ff5035
Nicholas Wilson <nicholas@nicholaswilson.me.uk>
2024-12-17 11:02:24 +0000

    Switch zig build to use b.addConfigHeader()

--- build.zig
+++ build.zig
@@ -12,9 +12,37 @@ pub fn build(b: *std.Build) !void {
     const linkage = b.option(std.builtin.LinkMode, "linkage", "whether to statically or dynamically link the library") orelse @as(std.builtin.LinkMode, if (target.result.isGnuLibC()) .dynamic else .static);
     const codeUnitWidth = b.option(CodeUnitWidth, "code-unit-width", "Sets the code unit width") orelse .@"8";
 
-    const copyFiles = b.addWriteFiles();
-    _ = copyFiles.addCopyFile(b.path("src/config.h.generic"), "config.h");
-    _ = copyFiles.addCopyFile(b.path("src/pcre2.h.generic"), "pcre2.h");
+    const pcre2_header_dir = b.addWriteFiles();
+    const pcre2_header = pcre2_header_dir.addCopyFile(b.path("src/pcre2.h.generic"), "pcre2.h");
+
+    const config_header = b.addConfigHeader(
+        .{
+            .style = .{ .cmake = b.path("config-cmake.h.in") },
+            .include_path = "config.h",
+        },
+        .{
+            .HAVE_ASSERT_H = true,
+            .HAVE_UNISTD_H = (target.result.os.tag != .windows),
+            .HAVE_WINDOWS_H = (target.result.os.tag == .windows),
+
+            .HAVE_MEMMOVE = true,
+            .HAVE_STRERROR = true,
+
+            .SUPPORT_PCRE2_8 = codeUnitWidth == CodeUnitWidth.@"8",
+            .SUPPORT_PCRE2_16 = codeUnitWidth == CodeUnitWidth.@"16",
+            .SUPPORT_PCRE2_32 = codeUnitWidth == CodeUnitWidth.@"32",
+            .SUPPORT_UNICODE = true,
+
+            .PCRE2_EXPORT = null,
+            .PCRE2_LINK_SIZE = 2,
+            .PCRE2_HEAP_LIMIT = 20000000,
+            .PCRE2_MATCH_LIMIT = 10000000,
+            .PCRE2_MATCH_LIMIT_DEPTH = "MATCH_LIMIT",
+            .PCRE2_MAX_VARLOOKBEHIND = 255,
+            .NEWLINE_DEFAULT = 2,
+            .PCRE2_PARENS_NEST_LIMIT = 250,
+        },
+    );
 
     // pcre2-8/16/32.so
 
@@ -30,18 +58,17 @@ pub fn build(b: *std.Build) !void {
     });
 
     lib.defineCMacro("HAVE_CONFIG_H", null);
-    lib.defineCMacro("HAVE_MEMMOVE", null);
     lib.defineCMacro("PCRE2_CODE_UNIT_WIDTH", @tagName(codeUnitWidth));
-    lib.defineCMacro("SUPPORT_UNICODE", null);
     if (linkage == .static) {
         lib.defineCMacro("PCRE2_STATIC", null);
     }
 
+    lib.addConfigHeader(config_header);
+    lib.addIncludePath(pcre2_header_dir.getDirectory());
     lib.addIncludePath(b.path("src"));
-    lib.addIncludePath(copyFiles.getDirectory());
 
     lib.addCSourceFile(.{
-        .file = copyFiles.addCopyFile(b.path("src/pcre2_chartables.c.dist"), "pcre2_chartables.c"),
+        .file = b.addWriteFiles().addCopyFile(b.path("src/pcre2_chartables.c.dist"), "pcre2_chartables.c"),
     });
 
     lib.addCSourceFiles(.{
@@ -77,7 +104,7 @@ pub fn build(b: *std.Build) !void {
         },
     });
 
-    lib.installHeader(b.path("src/pcre2.h.generic"), "pcre2.h");
+    lib.installHeader(pcre2_header, "pcre2.h");
     b.installArtifact(lib);
 
 
@@ -105,15 +132,14 @@ pub fn build(b: *std.Build) !void {
         });
 
         posixLib.defineCMacro("HAVE_CONFIG_H", null);
-        posixLib.defineCMacro("HAVE_MEMMOVE", null);
         posixLib.defineCMacro("PCRE2_CODE_UNIT_WIDTH", @tagName(codeUnitWidth));
-        posixLib.defineCMacro("SUPPORT_UNICODE", null);
         if (linkage == .static) {
             posixLib.defineCMacro("PCRE2_STATIC", null);
         }
 
+        posixLib.addConfigHeader(config_header);
+        posixLib.addIncludePath(pcre2_header_dir.getDirectory());
         posixLib.addIncludePath(b.path("src"));
-        posixLib.addIncludePath(copyFiles.getDirectory());
 
         posixLib.addCSourceFiles(.{
             .files = &.{
@@ -121,6 +147,7 @@ pub fn build(b: *std.Build) !void {
             },
         });
 
+        posixLib.installHeader(b.path("src/pcre2posix.h"), "pcre2posix.h");
         b.installArtifact(posixLib);
 
         pcre2test.linkLibrary(posixLib);
@@ -130,19 +157,10 @@ pub fn build(b: *std.Build) !void {
     // pcre2test (again)
 
     pcre2test.defineCMacro("HAVE_CONFIG_H", null);
-    pcre2test.defineCMacro("HAVE_MEMMOVE", null);
-    if (target.result.os.tag != .windows) {
-        pcre2test.defineCMacro("HAVE_UNISTD_H", null);
-    }
-    pcre2test.defineCMacro("HAVE_STRERROR", null);
-    pcre2test.defineCMacro("SUPPORT_UNICODE", null);
-    pcre2test.defineCMacro(b.fmt("SUPPORT_PCRE2_{s}", .{@tagName(codeUnitWidth)}), null);
-    if (linkage == .static) {
-        pcre2test.defineCMacro("PCRE2_STATIC", null);
-    }
 
+    pcre2test.addConfigHeader(config_header);
+    pcre2test.addIncludePath(pcre2_header_dir.getDirectory());
     pcre2test.addIncludePath(b.path("src"));
-    pcre2test.addIncludePath(copyFiles.getDirectory());
 
     pcre2test.addCSourceFile(.{
         .file = b.path("src/pcre2test.c"),
