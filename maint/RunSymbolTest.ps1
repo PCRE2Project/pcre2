ce6e960c4968c6f9a5e836bf575beffa3f636ce0
Nicholas Wilson <nicholas@nicholaswilson.me.uk>
2025-02-12 21:30:56 +0000

    Add tests using nm to check so contents (#693)
    We want to ensure that the symbols included in shared object files (.so, .dylib, .dll) are as expected.
    
    This verifies that our Autoconf and CMake builds are using the `-fvisibility` flags and attributes correctly.
    
    As it turns out, the CMake build on Solaris is not working, and was exposing internal symbols due to a CMake issue.

---
+++ maint/RunSymbolTest.ps1
@@ -0,0 +1,54 @@
+# Script to test that all the symbols of a DLL are as expected.
+
+param (
+    [Parameter(Mandatory=$true)]
+    [string]$inputDir,
+
+    [Parameter(Mandatory=$true)]
+    [string]$manifestDir
+)
+
+if ((-not $inputDir) -or (-not $manifestDir)) {
+  throw "Usage: .\RunSymbolTest.ps1 <dll_dir> <manifest_dir>"
+}
+
+$dllNames = @("pcre2-8", "pcre2-16", "pcre2-32", "pcre2-posix")
+
+foreach ($dllName in $dllNames) {
+    $expectedFile = Join-Path $manifestDir ("manifest-lib$dllName.so")
+    $dllFile = Join-Path $inputDir ("$dllName.dll")
+    $base = [System.IO.Path]::GetFileName($expectedFile)
+
+    # Get path to dumpbin using vswhere
+    $vswhere = "C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe"
+    $dumpbin = & $vswhere -latest -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -find VC\Tools\MSVC\*\bin\Hostx64\x64\dumpbin.exe | Select-Object -First 1
+
+    $actualSymbols = & $dumpbin /exports $dllFile |
+        ForEach-Object {
+            if ($_ -match '^\s*\d+\s+[0-9A-Fa-f]+\s+[0-9A-Fa-f]+\s+(\S+)') {
+                "T $($matches[1])"
+            }
+        } |
+        Where-Object {
+            $_ -match '^T '
+        } |
+        Sort-Object
+
+    $actualOutput = ($actualSymbols -join "`n") + "`n"
+    $null = New-Item -Force $base -Value $actualOutput
+
+    $expectedOutput = Get-Content -Path $expectedFile -Raw
+    $actualOutput = Get-Content -Path $base -Raw
+
+    if ($expectedOutput -ne $actualOutput) {
+        Write-Host "Shared object contents for $dllFile differ from expected"
+        Write-Host "===Actual==="
+        Write-Host $actualOutput
+        Write-Host "===End==="
+        throw "Symbol test failed"
+    } else {
+        Write-Host "Shared object contents for $dllFile match expected"
+    }
+
+    Remove-Item -Path $base -Force
+}
