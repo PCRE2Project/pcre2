d5e178a439b9a6c2da614ef1a77f75382b4677ba
Nicholas Wilson <nicholas@nicholaswilson.me.uk>
2025-10-08 13:05:47 +0100

    Fix x86 (32-bit) build; TMP3 can't be used as a source register

--- src/pcre2_jit_compile.c
+++ src/pcre2_jit_compile.c
@@ -13783,17 +13783,17 @@ if (common->has_set_som &&
   struct sljit_jump *bad_som_ahead;
   struct sljit_jump *fallthrough;
 
-  OP1(SLJIT_MOV, TMP1, 0, SLJIT_MEM1(SLJIT_SP), OVECTOR(0));
-  OP1(SLJIT_MOV, TMP2, 0, STR_PTR, 0);  /* STR_PTR, since OVECTOR(1) hasn't been populated yet */
   if (HAS_VIRTUAL_REGISTERS)
     {
-    OP1(SLJIT_MOV, TMP3, 0, ARGUMENTS, 0);
-    OP1(SLJIT_MOV, TMP3, 0, SLJIT_MEM1(TMP3), SLJIT_OFFSETOF(jit_arguments, str));
+    OP1(SLJIT_MOV, TMP1, 0, ARGUMENTS, 0);
+    OP1(SLJIT_MOV, TMP3, 0, SLJIT_MEM1(TMP1), SLJIT_OFFSETOF(jit_arguments, str));
     }
   else
     {
     OP1(SLJIT_MOV, TMP3, 0, SLJIT_MEM1(ARGUMENTS), SLJIT_OFFSETOF(jit_arguments, str));
     }
+  OP1(SLJIT_MOV, TMP1, 0, SLJIT_MEM1(SLJIT_SP), OVECTOR(0));
+  OP1(SLJIT_MOV, TMP2, 0, STR_PTR, 0);  /* STR_PTR, since OVECTOR(1) hasn't been populated yet */
 
   bad_som_behind = CMP(SLJIT_LESS, TMP1, 0, TMP3, 0);  /* (ovector[0] < jit_arguments->str)? */
   bad_som_ahead = CMP(SLJIT_GREATER, TMP1, 0, TMP2, 0);  /* (ovector[0] > ovector[1])? */
