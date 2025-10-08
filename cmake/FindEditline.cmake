0ee6302238faf8e99e3eb9ef8655a4c7b1f3f16c
Nicholas Wilson <nicholas@nicholaswilson.me.uk>
2025-10-08 18:22:27 +0100

    Modernise FindReadline.cmake, and fix on Solaris (#813)
    The CMake build fails on Solaris out of the box.
    
    * Fix Solaris vanilla CMake build; remove CI override that forces use of termcap.
    * Move up pcre2_use_system_extensions to the top of CMakeLists.txt
    * Use cmake_push_check_state() instead of manual management of state
    * Move _GNU_SOURCE detection into pcre2_use_system_extensions, to match Autoconf

--- cmake/FindEditline.cmake
+++ cmake/FindEditline.cmake
@@ -1,13 +1,10 @@
 # Modified from FindReadline.cmake (PH Feb 2012)
 
-if(EDITLINE_INCLUDE_DIR AND EDITLINE_LIBRARY)
-  set(EDITLINE_FOUND TRUE)
-else()
-  find_path(EDITLINE_INCLUDE_DIR readline.h PATH_SUFFIXES editline edit/readline)
+find_path(EDITLINE_INCLUDE_DIR readline.h PATH_SUFFIXES editline edit/readline)
+mark_as_advanced(EDITLINE_INCLUDE_DIR)
 
-  find_library(EDITLINE_LIBRARY NAMES edit)
-  include(FindPackageHandleStandardArgs)
-  find_package_handle_standard_args(Editline DEFAULT_MSG EDITLINE_INCLUDE_DIR EDITLINE_LIBRARY)
+find_library(EDITLINE_LIBRARY NAMES edit)
+mark_as_advanced(EDITLINE_LIBRARY)
 
-  mark_as_advanced(EDITLINE_INCLUDE_DIR EDITLINE_LIBRARY)
-endif()
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Editline DEFAULT_MSG EDITLINE_LIBRARY EDITLINE_INCLUDE_DIR)
